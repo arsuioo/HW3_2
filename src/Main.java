d5ea2a1fcbdc2d533d41a9002b66147b4a442761
Arsh_174 <74has@mail.ru>
2024-02-03 14:33:07 +0500

    bmiCalculate

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 65;
+        int height = (int)Math.round (1.78);
+        int index = service.calculate(weight, height);
+        System.out.println(index);
+    }
+}
